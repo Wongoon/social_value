0aa4288b21c75c14eaa1fde3fc09fb3987fca32a
Wongoon <rmsgh64@gmail.com>
2022-09-20 12:31:51 +0900

    code update

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,6 @@
 const body = document.querySelector('body');
 const sidebar = body.querySelector('nav');
 const toggle = body.querySelector(".toggle");
-const navlink = body.querySelectorAll('.nav-link');
-const tabContent = body.querySelectorAll('.tab-content');
 
 toggle.addEventListener("click", function(){
     sidebar.classList.toggle("close");
